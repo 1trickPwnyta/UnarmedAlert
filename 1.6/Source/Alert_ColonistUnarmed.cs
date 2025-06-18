b80b86990d1c90d52321682c86e880c8c45cbc88
1trickPwnyta <1trickPwnyta@kangaroostandard.com>
2025-06-17 18:54:25 -0700

    Update to 1.6

---
+++ 1.6/Source/Alert_ColonistUnarmed.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+using Verse;
+
+namespace UnarmedAlert
+{
+    public class Alert_ColonistUnarmed : Alert
+    {
+        private List<Pawn> GetCulprits()
+        {
+            List<Pawn> culprits = new List<Pawn>();
+            PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_FreeColonists.ForEach(pawn => 
+            {
+                if (pawn.equipment.Primary == null && (pawn.Spawned || pawn.IsCaravanMember()) && !pawn.InMentalState && (UnarmedAlertSettings.IncludeGuests || (!pawn.HasExtraMiniFaction() && !pawn.HasExtraHomeFaction())) && !pawn.Downed && !pawn.WorkTagIsDisabled(WorkTags.Violent) && pawn.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation) && (UnarmedAlertSettings.IncludeSlaves || !pawn.IsSlave) && (UnarmedAlertSettings.IncludeChildren || pawn.DevelopmentalStage == DevelopmentalStage.Adult)) culprits.Add(pawn);
+            });
+            return culprits;
+        }
+
+        public Alert_ColonistUnarmed()
+        {
+            this.defaultLabel = "UnarmedAlert_ColonistUnarmed".Translate();
+            this.defaultExplanation = "UnarmedAlert_ColonistUnarmedDesc".Translate();
+            this.defaultPriority = AlertPriority.High;
+        }
+
+        public override AlertReport GetReport()
+        {
+            return AlertReport.CulpritsAre(GetCulprits());
+        }
+    }
+}
---
+++ 1.6/Source/UnarmedAlertSettings.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace UnarmedAlert
+{
+    public class UnarmedAlertSettings : ModSettings
+    {
+        public static bool IncludeGuests = false;
+        public static bool IncludeSlaves = false;
+        public static bool IncludeChildren = true;
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+
+            listingStandard.Begin(inRect);
+
+            listingStandard.CheckboxLabeled("UnarmedAlert_IncludeGuests".Translate(), ref IncludeGuests);
+            if (ModsConfig.IdeologyActive)
+            {
+                listingStandard.CheckboxLabeled("UnarmedAlert_IncludeSlaves".Translate(), ref IncludeSlaves);
+            }
+            if (ModsConfig.BiotechActive)
+            {
+                listingStandard.CheckboxLabeled("UnarmedAlert_IncludeChildren".Translate(), ref IncludeChildren);
+            }
+
+            listingStandard.End();
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref IncludeGuests, "IncludeGuests", false);
+            Scribe_Values.Look(ref IncludeSlaves, "IncludeSlaves", false);
+            Scribe_Values.Look(ref IncludeChildren, "IncludeChildren", true);
+        }
+    }
+}
